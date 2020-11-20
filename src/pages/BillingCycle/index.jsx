739f588ca0589297df132df3ba19aff9f9d3385d
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-20 10:45:54 -0300

    Criado página BillingCycle

---
+++ src/pages/BillingCycle/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BillingCycle(props) {
+    return (
+        <div>
+            <h1>Billing Cycles</h1>
+        </div>
+    )
+}
+
+export default BillingCycle
