d2bcd2a9cf84437d502f9e6c2b238208d0c33328
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-20 15:46:01 -0300

    Adicionado componente ContentHeader em Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import ContentHeader from '../../components/ContentHeader'
 
 function Dashboard(props) {
     return (
         <div>
+            <ContentHeader title="Dashboard" small="Versão 1.0" />
             <h1>Dashboard</h1>
         </div>
     )
