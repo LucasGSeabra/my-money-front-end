2244defa81f151c37a03fc7448a0889e07b8ebf1
Lucas Seabra <lucasgseabra@gmail.com>
2020-11-20 09:14:22 -0300

    Adicionado componente Footer em App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import PageHeader from './template/Header/'
 import Sidebar from './template/Sidebar/'
+import Footer from './template/Footer/'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <div class="grid-container">
-        <Sidebar />
         <PageHeader />
+        <Sidebar />
         <div className="content" />
+        <Footer />
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
