dd7439d5891e9b955ae9f79c41c88572bd84cde9
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-22 13:33:00 -0300

    Adicionado Card em Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import ContentHeader from '../../components/ContentHeader'
 import Content from '../../components/Content'
+import CardComponent from '../../components/Card'
 import { Cash, Card, Wallet } from '@styled-icons/ionicons-outline'
 
 const CashIcon = styled(Cash)`
@@ -27,7 +28,15 @@ function Dashboard(props) {
         <div>
             <ContentHeader title="Dashboard" small="Versão 1.0" />
             <Content>
-                Dashboard
+                <CardComponent theme="cred" val="35" title="Total de Créditos">
+                <CashIcon />
+                </CardComponent>
+                <CardComponent theme="deb" val="35" title="Total de Débitos">
+                <CardIcon />
+                </CardComponent>
+                <CardComponent theme="cons" val="35" title="Total Consolidado">
+                <WalletIcon />
+                </CardComponent>
             </Content>
         </div>
     )
