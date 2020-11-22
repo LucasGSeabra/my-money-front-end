4d7e28ef75f359c7411d64c4fed08f24f6b2865b
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-22 15:37:34 -0300

    Modificado index para configurar provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 import App from './App';
+import reducers from './reducers'
+
+const store = applyMiddleware(promise)(createStore)(reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <Provider store={store}> 
+        <App />
+      </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
