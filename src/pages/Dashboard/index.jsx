d0797e41a4995b920c481c5a37dd550e853f772d
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-22 13:32:45 -0300

    Adicionado ícones em Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 import ContentHeader from '../../components/ContentHeader'
 import Content from '../../components/Content'
+import { Cash, Card, Wallet } from '@styled-icons/ionicons-outline'
+
+const CashIcon = styled(Cash)`
+    opacity: 30%;
+    width: 72px;
+    height: 72px;  
+`
+
+const CardIcon = styled(Card)`
+    opacity: 30%;
+    width: 72px;
+    height: 72px;  
+`
+
+const WalletIcon = styled(Wallet)`
+    opacity: 30%;
+    width: 72px;
+    height: 72px;  
+`
 
 function Dashboard(props) {
     return (
