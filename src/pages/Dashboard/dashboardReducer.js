f784bfec0b6440d94ab65aa375d70c497bd1d415
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-25 20:24:08 -0300

    Corrigido erro ao iniciar reducer

--- src/pages/Dashboard/dashboardReducer.js
+++ src/pages/Dashboard/dashboardReducer.js
@@ -4,6 +4,8 @@ export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'BILLING_SUMMARY_FETCHED':
             return { ...state, sumary: action.payload.data }
+        default:
+            return state
     }
     
 }
