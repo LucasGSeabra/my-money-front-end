c180f9b9c2317696e0d9e3ad05534f923a5f3ef2
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-29 11:55:50 -0300

    Modificado BrowserRouter para HashRouter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 import Dashboard from './pages/Dashboard'
 import BillingCycle from './pages/BillingCycle'
 
 function Routes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Route path="/" exact component={Dashboard} />
             <Route path="/billingCycles" component={BillingCycle} />
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
