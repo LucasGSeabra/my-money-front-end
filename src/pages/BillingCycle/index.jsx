aafe658cc72ca6eed5e0d5888c6cb5417cc0f9af
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-29 16:22:49 -0300

    Adicionado List em BillingCycle

--- src/pages/BillingCycle/index.jsx
+++ src/pages/BillingCycle/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ContentHeader from '../../components/ContentHeader/'
 import Content from '../../components/Content/'
-import Tabs from '../../components/Tabs/'
+import TabsContainer from '../../components/TabsContainer/'
+import Tab from '../../components/TabContent/'
+import List from '../../components/List/'
 
 function BillingCycle(props) {
+
     return (
         <div>
             <ContentHeader title="Billing Cycles" small="Cadastro" />
             <Content>
-                <Tabs></Tabs>
+                <TabsContainer>
+                    <Tab key="1" title="list">
+                        <List />
+                    </Tab>
+                    <Tab key="2" title="Cadastro">
+                        Teste
+                    </Tab>
+                </TabsContainer>
             </Content>
         </div>
     )
