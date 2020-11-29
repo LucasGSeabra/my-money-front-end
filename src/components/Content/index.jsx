4ae088475204283c57e5e706adec7cc3403efc18
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-29 11:58:11 -0300

    Modificado estilização de contentContainer

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 const ContentContainer = styled.div`
     display: flex;
     flex-direction: row;
+    background-color: rgba(255, 255, 255);
+    margin: 0px 10px;
 `
 
 function Content(props) {
