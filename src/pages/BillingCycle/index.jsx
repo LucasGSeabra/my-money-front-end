2d24d503e1f85c8c5729ebe1a9db42340f98fca8
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-11-29 12:16:49 -0300

    Modificado BillingCycle

--- src/pages/BillingCycle/index.jsx
+++ src/pages/BillingCycle/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ContentHeader from '../../components/ContentHeader/'
+import Content from '../../components/Content/'
+import Tabs from '../../components/Tabs/'
 
 function BillingCycle(props) {
     return (
         <div>
-            <h1>Billing Cycles</h1>
+            <ContentHeader title="Billing Cycles" small="Cadastro" />
+            <Content>
+                <Tabs></Tabs>
+            </Content>
         </div>
     )
 }
