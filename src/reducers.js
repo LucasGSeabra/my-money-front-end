a87607626df5518735137a59869d9740f4659930
Lucas Seabra <lucasgseabra@gmail.com>
2020-12-01 16:51:36 -0300

    Adicionado reducer form

--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux'
 import dashboardReducer from './pages/Dashboard/dashboardReducer'
 import billingCycleReducer from './pages/BillingCycle/billingCyclesReducer'
-
+import { reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    billingCycle: billingCycleReducer
+    billingCycle: billingCycleReducer,
+    form: formReducer
 })
 
 export default rootReducer
