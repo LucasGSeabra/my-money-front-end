086c2895f8e90cfbabe3690cafd7116dfca4cf84
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-03 20:28:06 -0300

    Adicionado toastr em rootReducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import dashboardReducer from './pages/Dashboard/dashboardReducer'
 import billingCycleReducer from './pages/BillingCycle/billingCyclesReducer'
 import { reducer as formReducer} from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     billingCycle: billingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
