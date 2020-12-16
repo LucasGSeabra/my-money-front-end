013316cc0f128847ebe202f546693300b079ab7b
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-16 15:17:50 -0300

    Modificado estilização de Form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import styled from 'styled-components'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 import { reduxForm, Field } from 'redux-form'
 
-function FormComponent() {
+const FormContainer = styled(Form)`
+    width: 100%;
+    height: 100%;
+    padding 15px 0px 10px 15px ;
+`
+
+function FormComponent(props) {
     return (
         <div>
-            <Form>
-                <Field name="name" component="input"/>
-                <Field name="month" component="input"/>
-                <Field name="year" component="input"/>
+            <FormContainer onSubmit={(e) => props.handleSubmit(e)}>
+                <Form.Group>
+                    <Form.Row>
+                        <Col lg={3} sm={12}>
+                            <Form.Label>Nome</Form.Label>
+                            <Field name="name" component={Form.Control}/>
+                        </Col>
+                        <Col lg={3} sm={12}>
+                            <Form.Label>Mês</Form.Label>
+                            <Field name="month" component={Form.Control}/>
+                        </Col>
+                        <Col lg={3} sm={12}>
+                            <Form.Label>Ano</Form.Label>
+                            <Field name="year" component={Form.Control}/>
+                        </Col>  
+                    </Form.Row>
+                </Form.Group>
                 <Button variant="primary" type="submit">Salvar</Button>
-            </Form>
+            </FormContainer>
         </div>
     )
 }
