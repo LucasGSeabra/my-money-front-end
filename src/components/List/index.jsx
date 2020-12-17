f6fb55774ad798e906e009eda7c635c7ab4e9f3d
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-17 11:02:09 -0300

    Adicionado editBillingCycle

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react'
-import { Table } from 'react-bootstrap'
+import styled from 'styled-components'
+import { Table, Button } from 'react-bootstrap'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { getList, initializeForm } from '../../pages/BillingCycle/billingCycleActions'
 import { getList } from '../../pages/BillingCycle/billingCycleActions'
 
 function List(props) {
 
     const list = props.list || []
 
+    function editBillingCycle(billingCycle) {
+        props.changeTab(3)
+        props.initializeForm(billingCycle)     
+    }
+
     useEffect(() => {
         props.getList()
-        console.log(props.list)
     }, []) 
 
     return(
@@ -21,6 +27,7 @@ function List(props) {
                         <th>Nome</th>
                         <th>Mês</th>
                         <th>Ano</th>
+                        <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,6 +36,9 @@ function List(props) {
                             <td>{bc.name}</td>
                             <td>{bc.month}</td>
                             <td>{bc.year}</td>
+                            <td><Button variant="warning" onClick={() => editBillingCycle(bc)}>
+                                </Button>
+                            </td>
                         </tr>
                     )) }
                 </tbody>
@@ -38,5 +48,5 @@ function List(props) {
 }
 
 const mapStateToProps = state => ({ list: state.billingCycle.list })
-const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ getList, initializeForm }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(List)
