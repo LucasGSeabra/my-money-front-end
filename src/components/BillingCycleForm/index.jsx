b96b2b4ce424130d65ac85f0399402c8ff76e258
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-22 19:55:27 -0300

    Renomeado Form para BillingCycleForm

--- src/components/BillingCycleForm/index.jsx
+++ src/components/BillingCycleForm/index.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Form, Button, Col } from 'react-bootstrap'
-import { reduxForm, Field } from 'redux-form'
 
 const FormContainer = styled(Form)`
     width: 100%;
     height: 100%;
-    padding 15px 0px 10px 15px ;
 `
 
-const ReduxFormControl = ({input, meta, ...props}) => {
-    return <Form.Control {...props} {...input} />
-};
-
-function FormComponent(props) {
     
     const variant = props.buttonTitle === 'Cadastrar'
         ? 'primary'
@@ -26,18 +19,12 @@ function FormComponent(props) {
             <FormContainer onSubmit={(e) => props.handleSubmit(e)}>
                 <Form.Group>
                     <Form.Row>
-                        <Col lg={3} sm={12}>
                             <Form.Label>Nome</Form.Label>
-                            <Field name="name" component={ReduxFormControl} readOnly={props.readOnly} />
                         </Col>
-                        <Col lg={3} sm={12}>
                             <Form.Label>Mês</Form.Label>
-                            <Field name="month" component={ReduxFormControl} readOnly={props.readOnly} />
                         </Col>
-                        <Col lg={3} sm={12}>
                             <Form.Label>Ano</Form.Label>
-                            <Field name="year" component={ReduxFormControl} readOnly={props.readOnly} />
-                        </Col>  
+                        </Col>
                     </Form.Row>
                 </Form.Group>
                 <Button variant={variant} type="submit">{ props.buttonTitle }</Button>
@@ -46,4 +33,3 @@ function FormComponent(props) {
     )
 }
 
-export default reduxForm({form: 'billingCycleForm', destroyOnUnmount: false })(FormComponent)
