4a3910fb93856d595a587fd40c9ea6b515395fa8
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-22 20:06:05 -0300

    Substituido Field por Input

--- src/components/BillingCycleForm/index.jsx
+++ src/components/BillingCycleForm/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Form, Button, Col } from 'react-bootstrap'
+import { reduxForm } from 'redux-form'
+import Input from '../Input'
 
 const FormContainer = styled(Form)`
     width: 100%;
     height: 100%;
+    padding 15px 15px 10px 15px ;
 `
 
+function BillingCycleForm(props) {
     
     const variant = props.buttonTitle === 'Cadastrar'
         ? 'primary'
@@ -19,11 +23,17 @@ const FormContainer = styled(Form)`
             <FormContainer onSubmit={(e) => props.handleSubmit(e)}>
                 <Form.Group>
                     <Form.Row>
+                        <Col lg={4} sm={12}>
                             <Form.Label>Nome</Form.Label>
+                            <Input name="name" readOnly={props.readOnly} />
                         </Col>
+                        <Col lg={4} sm={12}>
                             <Form.Label>Mês</Form.Label>
+                            <Input name="month" readOnly={props.readOnly} />
                         </Col>
+                        <Col lg={4} sm={12}>
                             <Form.Label>Ano</Form.Label>
+                            <Input name="year" readOnly={props.readOnly} />
                         </Col>
                     </Form.Row>
                 </Form.Group>
@@ -33,3 +43,4 @@ const FormContainer = styled(Form)`
     )
 }
 
+export default reduxForm({form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
