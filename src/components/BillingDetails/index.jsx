0e314d4b5cb0a2271515125469d1c42d70250896
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-23 16:35:05 -0300

    Adicionado listagem de créditos e débitos

--- src/components/BillingDetails/index.jsx
+++ src/components/BillingDetails/index.jsx
@@ -1,23 +1,51 @@
 import React from 'react' 
-import { Table } from 'react-bootstrap'
+import styled from 'styled-components'
+import { Table, Button } from 'react-bootstrap'
 import Input from '../Input'
+import { Remove } from '@styled-icons/ionicons-outline'
+
+const RemoveIcon = styled(Remove)`
+    width: 16px;
+    height: 16px;  
+    color: #fff
+`
 
 function BillingDetails(props){
+
+    const detailsList = props.detailsList || []
+
+    const title = props.type === 'credits' ? 'Créditos' : 'Débitos'
     return (
         <div>
-            <h5>{props.title}</h5>
+            <h5>{title}</h5>
             <Table>
                 <thead>
                     <tr>
                         <th>Nome</th>
                         <th>Valor</th>
+                        { props.type === 'debts' &&
+                            <th>Status</th>
+                        }
                         <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <td><Input name="nome" placeholder="Nome" /></td>
-                    <td><Input name="valor" placeholder="Valor" /></td>
+                   { detailsList.map((item, index) => (
+                       <tr key={index}>
+                            <td><Input name={`${props.type}[${index}].name`} placeholder="Nome" /></td>
+                            <td><Input name={`${props.type}[${index}].value`} placeholder="Valor" /></td>
+                            { props.type === 'debts' &&
+                                <td><Input name={`${props.type}[${index}].status`} placeholder="Status" /></td>
+                            }
+                            <td>
+                                <Button variant="danger" onClick={() => removeItem(index)}><RemoveIcon/></Button>
+                            </td>
+                       </tr>
+                   ))}                    
                 </tbody>
+                <tfoot>
+                    <Button variant="success" onClick={() => addItem()}>Adicionar {title}</Button>
+                </tfoot>
             </Table>
         </div>
     )
