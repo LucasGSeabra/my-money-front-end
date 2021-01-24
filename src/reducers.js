980100c921807f34038277600116396772390844
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-24 13:36:06 -0300

    Adicionado loginReducer a reducers

--- src/reducers.js
+++ src/reducers.js
@@ -3,12 +3,14 @@ import dashboardReducer from './pages/Dashboard/dashboardReducer'
 import billingCycleReducer from './pages/BillingCycle/billingCyclesReducer'
 import { reducer as formReducer} from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
+import loginReducer from ' ./pages/Login/loginReducer'
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     billingCycle: billingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    login: loginReducer
 })
 
 export default rootReducer
