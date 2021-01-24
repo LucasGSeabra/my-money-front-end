bbabbe61d1f6a0b08440fe5900a2a97d4b4a61b0
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-24 13:36:24 -0300

    Criado actions de login

---
+++ src/pages/Login/loginActions.js
@@ -0,0 +1,46 @@
+import { toastr } from 'react-redux-toastr'
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3003/oapi'
+
+function submit(url, values) {
+    return dispatch => {
+        axios.post(url, values)
+            .then(response => {
+                dispatch([
+                    { type: 'USER_FETCHED', payload: response.data }
+                ])
+            })
+            .catch(err => {
+                err.response.data.errors.forEach(
+                    error => toastr.error('Erro', error)
+                )
+            })
+    }
+}
+
+export function login(values) {
+    return submit(values, `${BASE_URL}/login`)
+}
+
+export function signup(values) {
+    return submit(values, `${BASE_URL}/signup`)
+}
+
+export function logout() {
+    return { type: 'TOKEN_VALIDATED', payload: false }
+}
+
+export function validateToken(token) {
+    return dispatch => {
+        if(token) {
+            axios.post(`${BASE_URL}/validateToken`, { token })
+                .then(response => {
+                    dispatch({ type: 'TOKEN_VALIDATED', payload: response.data.valid })
+                })
+                .catch(error => dispatch({ type: 'TOKEN_VALIDATED', payload: false }))
+        } else {
+            dispatch({ type: 'TOKEN_VALIDATED', payload: false })
+        }
+    }
+}
