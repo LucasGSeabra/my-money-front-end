b47b7b30fff4235a8de2e0bca07112a147bc25f1
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-25 18:33:22 -0300

    Adicionado verificação de login em app

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,34 @@ import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
 import Alert from './components/Alert'
 import Routes from './routes'
+import { connect } from 'react-redux'
+import Login from './pages/Login/'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="grid-container">
-        <PageHeader />
-        <Sidebar />
-        <div className="content">
-          <Routes />
+function App(props) {
+  if(props.validToken) {
+    return (
+      <div className="App">
+        <div className="grid-container">
+          <PageHeader />
+          <Sidebar />
+          <div className="content">
+            <Routes />
+          </div>
+          <Footer />
+          <Alert />
         </div>
-        <Footer />
-        <Alert />
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <Login />
+    )
+  }
+  
 }
 
-export default App;
+const mapStateToProps = state => ({ validToken: state.login.validToken })
+export default connect(mapStateToProps, null)(App)
