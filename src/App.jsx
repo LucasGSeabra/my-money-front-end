f5e1880021029e741b2e9e1e7b7fd09cb4c3d4f8
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-29 19:57:49 -0300

    Adicionado validação de usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import PageHeader from './components/Header'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
@@ -5,12 +6,23 @@ import Alert from './components/Alert'
 import Routes from './routes'
 import { connect } from 'react-redux'
 import Login from './pages/Login/'
+import { validateToken } from './pages/Login/loginActions'
+import { bindActionCreators } from 'redux'
+import axios from 'axios'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App(props) {
-  if(props.validToken) {
+
+  useEffect(() => {
+    if(props.validToken) {
+      props.validateToken(props.user.token)
+    } 
+  }, [])
+
+  if(props.user && props.validToken) {
+    axios.defaults.headers.common['authorization'] = props.user.token
     return (
       <div className="App">
         <div className="grid-container">
@@ -26,11 +38,14 @@ function App(props) {
     )
   } else {
     return (
-      <Login />
+      <div className="App">
+        <Login />
+        <Alert />
+      </div>
     )
-  }
-  
+  }   
 }
 
-const mapStateToProps = state => ({ validToken: state.login.validToken })
-export default connect(mapStateToProps, null)(App)
+const mapStateToProps = state => ({ validToken: state.login.validToken, user: state.login.user })
+const mapDispatchToProps = dispatch => bindActionCreators({ validateToken }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
